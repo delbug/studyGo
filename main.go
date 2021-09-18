0f3d366d8197a0efc51b1a47b13ae7f347167c91
吴江 <81692959@qq.com>
2021-09-18 17:59:01 +0800

    6-函数

--- main.go
+++ main.go
@@ -1,69 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-/*	map:类似于其他语言字典，或者哈希表的东西表现为 key-value
-
-      m    := map[int]bool{}
-	变量名 := map[key的类型] value的类型{}
-
+/*
+	函数：func
+	函数首字母大写可以被别的包调用，小写不可以被别的包调用
 */
 
 func main() {
-	fmt.Println("--------------------------")
-	// m2 := make(map[string]string)
+	fmt.Println("[函数] ↓↓↓ ********************************")
+	a(0, "str字符串0") // data1: 0 ,data2: str字符串0
+	a(2, "str字符串2") // data1: 2 ,data2: str字符串2
+	fmt.Println("[函数] ↑↑↑ ********************************")
 
-	var m map[string]string
-	m = map[string]string{} //  这种 声明一定要赋值
-	m["name"] = "张三"
-	m["sex"] = "男生"
-	fmt.Println(m) // map[name:张三 sex:男生]
-	fmt.Println("string{} ↑↑↑--------------------------")
+	a, b := swap("Google", "Runoob")
+	fmt.Println(a, b) // Runoob Google
+	fmt.Println("[] ↑↑↑ ********************************")
 
-	m1 := map[string]string{}
-	m1["name"] = "李四"
-	m1["sex"] = "女生"
-	fmt.Println(m1) // map[name:张三 sex:男生]
-	fmt.Println("string{} ↑↑↑--------------------------")
+	c, d := funcNum("1", "strrrr")
+	fmt.Println(c, d) // 1 等于: 1
+	fmt.Println("[funcNum] ↑↑↑ ********************************")
 
-	m2 := map[int]bool{}
-	m2[1] = true
-	m2[2] = false
-	fmt.Println(m2) // map[1:true 2:false]
-	fmt.Println("bool{} ↑↑↑--------------------------")
+	e, f := funcStr(1, 2)
+	fmt.Println(e, f) // 2  !== 1
+	fmt.Println("[funcStr] ↑↑↑ ********************************")
 
-	m3 := map[int]interface{}{}
-	m3[1] = 123
-	m3[2] = false
-	m3[3] = "strrr"
-	m3[4] = []int{1, 2, 3}
-	fmt.Println(m3) //	 map[1:123 2:false 3:strrr 4:[1 2 3]]
-	fmt.Println("interface ↑↑↑--------------------------")
+	g, h := funcName("张三", "李四")
+	fmt.Println(g, h) // 张三1 李四2
+	fmt.Println("[funcName] ↑↑↑ ********************************")
 
-	m4 := map[int]interface{}{}
-	m4[1] = 123
-	m4[2] = false
-	m4[3] = "strrr"
-	m4[4] = []int{1, 2, 3}
-	delete(m4, 4)                // 	删除第四项
-	fmt.Println("删除之后", m4)      // 	map[1:123 2:false 3:strrr]
-	fmt.Println("m4长度", len(m4)) // 	map[1:123 2:false 3:strrr]
-	fmt.Println("delete ↑↑↑--------------------------")
-
-	m5 := map[string]interface{}{}
-	m5["a"] = 123
-	m5["b"] = false
-	m5["c"] = "strrr"
-	m5["d"] = []int{1, 2, 3}
-
-	for k, v := range m5 {
-		fmt.Println(k, v)
+	niming := func(data string) {
+		fmt.Println(data)
 	}
-	fmt.Println("range 循环数组 ↑↑↑--------------------------")
+	niming("是匿名函数-") // 是匿名函数-
+	fmt.Println("[我是匿名函数] ^^^ ****************************")
 
-	// fmt.Println("[] ↓↓↓ ********************************")
-	// fmt.Println("[] ↑↑↑ ********************************")
+	budingxiang(1, "2", "3", "4", "5") // 1 [2 3 4 5]
+	// 类似于 arguments
+	fmt.Println("[不定向参数] ^^^ ****************************")
 
+}
+
+func a(data1 int, data2 string) {
+	fmt.Println("data1:", data1, ",data2:", data2)
+	if data1 > 1 {
+		fmt.Println(data1)
+	} else {
+		fmt.Println(data2)
+	}
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func funcNum(x string, y string) (string, string) {
+	if x == "1" {
+		return x, "等于: " + x
+	} else {
+		return y, "不等于1,为: " + y
+	}
+}
+
+func funcStr(x int, y int) (int, string) {
+	if x > 1 {
+		return x, ">1"
+	} else {
+		return y, " !== 1"
+	}
+}
+
+func funcName(x string, y string) (a string, b string) {
+	a = x + "1"
+	b = y + "2"
+	return
+}
+
+// 不定向参数
+func budingxiang(x int, y ...string) {
+	fmt.Println(x, y)
 }
 
 // fmt.Println("[] ↓↓↓ ********************************")
